eb7070f69aeeb217da39269b92e83c6cc9f8c6dd
vagosalv <vagosalvgd@gmail.com>
2017-11-03 12:40:15 +0200

    Add files via upload

---
+++ myflaskapp/data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Question One',
+            'body' : 'losadasdasdasdasdasda',
+            'author' : 'Vagos Alv',
+            'create_date' : '15/8/2017'
+        },
+        {
+            'id' : 2,
+            'title' : 'Question two',
+            'body' : 'asdg;jbfsg;jblgfas',
+            'author' : 'Vagos Alv',
+            'create_date' : '16/8/2017'
+        }
+    ]
+    return articles
